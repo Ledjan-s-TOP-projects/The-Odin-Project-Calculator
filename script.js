20ff7d1bd887450acbad81de959c9dddb461c4d2
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-15 17:12:32 +0200

    operate function implemented

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//----Variables for updating the display-----
+let num1;
+let operator;
+let num2;
+
 //-------Functions-----------
 function add(...numbers) {
   let total = 0;
@@ -8,15 +13,15 @@ function add(...numbers) {
 }
 
 function subtract(...numbers) {
-  let total = 0;
-  for (num of numbers) {
-    total -= num;
+  let total = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    total -= numbers[i];
   }
   return total;
 }
 
 function multiply(...numbers) {
-  let total = 0;
+  let total = 1;
   for (num of numbers) {
     total *= num;
   }
@@ -24,9 +29,24 @@ function multiply(...numbers) {
 }
 
 function divide(...numbers) {
-  let total = 0;
-  for (num of numbers) {
-    total /= num;
+  let total = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    total /= numbers[i];
   }
   return total;
+}
+
+function operate(operator, ...numbers) {
+  switch (operator) {
+    case "+":
+      return add(...numbers);
+    case "-":
+      return subtract(...numbers);
+    case "*":
+      return multiply(...numbers);
+    case "/":
+      return divide(...numbers);
+    default:
+      return "Invalid operator";
+  }
 }

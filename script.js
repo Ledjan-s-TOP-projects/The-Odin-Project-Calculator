4aef13c1baab1f19c07870dc03a4c6816145adb8
Ledjan Miçani <155323267+Choopin86@users.noreply.github.com>
2025-08-15 16:48:04 +0200

    subtract, multiply, divide functions completed

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+//-------Functions-----------
 function add(...numbers) {
   let total = 0;
   for (num of numbers) {
     total += num;
+  }
+  return total;
+}
+
+function subtract(...numbers) {
+  let total = 0;
+  for (num of numbers) {
+    total -= num;
+  }
+  return total;
+}
+
+function multiply(...numbers) {
+  let total = 0;
+  for (num of numbers) {
+    total *= num;
+  }
+  return total;
+}
+
+function divide(...numbers) {
+  let total = 0;
+  for (num of numbers) {
+    total /= num;
   }
   return total;
 }
